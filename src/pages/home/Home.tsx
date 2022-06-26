94d70a8114b80a90851e94511dcf4128660cebb8
Anthony G <agithaka@live.com>
2022-06-26 22:41:09 +0300

    Home Page Boilerplate

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Default } from '../../containers/layouts'
+
+const Home = () => {
+    return (
+        <Default>
+            <div>Home</div>
+        </Default>
+    )
+}
+
+export default Home
