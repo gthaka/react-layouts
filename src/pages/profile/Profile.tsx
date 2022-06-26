15e70d0f43b7def17fe3e2d39612c39c83c9cc05
Anthony G <agithaka@live.com>
2022-06-26 22:40:44 +0300

    Profile Page Boilerplate

---
+++ src/pages/profile/Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Default } from '../../containers/layouts'
+
+const Profile = () => {
+  return (
+    <Default>
+        <div>Profile</div>
+    </Default>
+  )
+}
+
+export default Profile
