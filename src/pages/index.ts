14393cb11e06f2fe5bac9e62aa3d6c43629e2852
Anthony G <agithaka@live.com>
2022-06-26 22:43:10 +0300

    Main Page index

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+import NotFound from './error/404'
+import { Home } from './home'
+import { Profile } from './profile'
+import {Settings} from './settings'
+
+export {NotFound,Home,Profile,Settings}
