2387f76020221df3cdb97183ba4697e62a2555e3
Anthony G <agithaka@live.com>
2022-06-26 22:45:21 +0300

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
-import App from "./containers/app";
+// import App from "./containers/app";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home, NotFound,Profile,Settings } from "./pages";
+
+// export default App
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path='profile' element={<Profile />} />
+                <Route path='settings' element={<Settings />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+// const App = Routing;
 
 export default App
