af43bb8c9324c098dbb1c9689d511dca6a993554
Anthony G <agithaka@live.com>
2022-06-26 19:08:04 +0300

    Two Tone MUI

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,4 +1,4 @@
-import { HomeOutlined, ThreeDRotation, Battery1Bar, Battery0Bar, Battery20, Battery50, Battery80, BatteryFull, FoodBankRounded, } from '@mui/icons-material';
+import { HomeOutlined, ThreeDRotation, FoodBankRounded, Battery0BarTwoTone, Battery1BarTwoTone, Battery20TwoTone, Battery50TwoTone, Battery80TwoTone, BatteryFullTwoTone, } from '@mui/icons-material';
 
 interface INav {
     [key:string]:{id?:string, name: string, link: string, icon:any;}[]
@@ -8,12 +8,12 @@ export const NAVI:INav = {
     main: [
         { id: 'home', name: "Home", link: "/", icon: (props?: any) => (<HomeOutlined {...props} />), },
         { id: 'i1', name: "Item 1", link: "/item1", icon: (props?: any) => (<ThreeDRotation {...props} />), },
-        { id: 'i2', name: "Item 2", link: "/item2", icon: (props?: any) => (<Battery0Bar {...props} />), },
-        { id: 'i3', name: "Item 3", link: "/item3", icon: (props?: any) => (<Battery1Bar {...props} />), },
-        { id: 'i4', name: "Item 4", link: "/item4", icon: (props?: any) => (<Battery20 {...props} />), },
-        { id: 'i5', name: "Item 5", link: "/item5", icon: (props?: any) => (<Battery50 {...props} />), },
-        { id: 'i6', name: "Item 6", link: "/item6", icon: (props?: any) => (<Battery80 {...props} />), },
-        { id: 'i7', name: "Item 7", link: "/item6", icon: (props?: any) => (<BatteryFull {...props} />), },
+        { id: 'i2', name: "Item 2", link: "/item2", icon: (props?: any) => (<Battery0BarTwoTone {...props} />), },
+        { id: 'i3', name: "Item 3", link: "/item3", icon: (props?: any) => (<Battery1BarTwoTone {...props} />), },
+        { id: 'i4', name: "Item 4", link: "/item4", icon: (props?: any) => (<Battery20TwoTone {...props} />), },
+        { id: 'i5', name: "Item 5", link: "/item5", icon: (props?: any) => (<Battery50TwoTone {...props} />), },
+        { id: 'i6', name: "Item 6", link: "/item6", icon: (props?: any) => (<Battery80TwoTone {...props} />), },
+        { id: 'i7', name: "Item 7", link: "/item6", icon: (props?: any) => (<BatteryFullTwoTone {...props} />), },
     ],
     footer: [
         { name: "FT Item", link: "/ft", icon: (props: any) => (<FoodBankRounded {...props} />), },
