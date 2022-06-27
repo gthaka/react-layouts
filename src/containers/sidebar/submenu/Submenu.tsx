f4453e6b1c101f940e0c2984d1b0ebaadd712069
Anthony G <agithaka@live.com>
2022-06-27 23:17:10 +0300

    added menu ctx provider - minor fixes

--- src/containers/sidebar/submenu/Submenu.tsx
+++ src/containers/sidebar/submenu/Submenu.tsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom';
+import { MenuContext } from '../../../components/context/MenuContextProvider';
 import { SUB_NAVI } from '../../../config'
 
 interface IProps {
     className: string;
-    menu?: { id: string, name: string; };
+    hasSub?: boolean;
+    // menu?: { id: string, name: string; };
 }
 
-const Submenu: React.FC<IProps | any> = props => {
+const Submenu: React.FC<IProps | boolean | any> = props => {
     // c
     // const {id, name} = props.menu;
+    const { menu } = useContext(MenuContext)
     return (
-        props.menu &&
+        props.hasSub &&
         <div className='border-r-2 border-gray-300'>
             <aside className='flex flex-col w-56 h-screen'>
                 <div className='px-2.5 pt-4 font-nzito sticky top-0 z-40 w-full bg-gray-100'>
-                    <h3 className='pb-4 pl-4 border-b-2 capitalize'>{props.menu.name}</h3>
+                    <h3 className='pb-4 pl-4 border-b-2 capitalize'>{menu}</h3>
                 </div>
                 {/* overflow auto */}
                 <div className='flex px-6 overflow-y-auto scrollbar'>
                     <ul className='list-none space-y-1.5 py-2 pl-4'>
-                        {SUB_NAVI[props.menu.id].map(i => (
+                        {SUB_NAVI[menu].map(i => (
                             <li key={i.name} >
                                 <NavLink to={i.link} className="border-1 border-gray-400">
                                     {i.name}
